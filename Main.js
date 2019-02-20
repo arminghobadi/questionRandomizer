379694a54b3911fb1e421b620dbdf606a884a98a
Dusty-Willow <muhammad.adeel.zafar@gmail.com>
2019-02-20 14:30:51 -0500

    Currently errors in questions after first question

--- Main.js
+++ Main.js
@@ -2,7 +2,9 @@ const fs = require('fs')
 
 var content;
 var array;
-var questions;
+var questions = new Array();
+var x, y;
+
 fs.readFile('./questions.txt', 'utf8', function read(err, data) {
   if (err) {
     throw err;
@@ -10,17 +12,25 @@ fs.readFile('./questions.txt', 'utf8', function read(err, data) {
   content = data;
   array = content.split('\n')
 
+  for (i = 0; i < array.length; i++)
+  {
+    if (Number.isInteger(parseInt(array[i].charAt(0))) == true)
+    {
+      x = array[i];
+      y = x;
+      questions.push(x);
+    }
+    else
+    {
+      y = x;
+      x += array[i];
+      questions.splice(y, 1, x);
+    }
+  }
+
   processFile();
 });
 
 function processFile () {
-  console.log(array[0]);
-}
-
-for (i = 0; i < array.length(); i++)
-{
-  if (Number.isInteger(array[i].charAt(0)) == true)
-  {
-    
-  }
+  console.log(questions);
 }
