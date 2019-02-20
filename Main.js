8d9267348cea69b8db97a602c097a62ce2e394e2
Dusty-Willow <muhammad.adeel.zafar@gmail.com>
2019-02-20 16:19:13 -0500

    Questions updated and Commented out for loop (Work in progress).

--- Main.js
+++ Main.js
@@ -6,7 +6,8 @@ var questions = new Array();
 var x, y;
 var answers = new Array();
 
-fs.readFile('./questions.txt', 'utf8', function read(err, data) {
+
+fs.readFile("./questions.txt", 'utf8', function read(err, data) {
   if (err) {
     throw err;
   }
@@ -35,6 +36,40 @@ fs.readFile('./questions.txt', 'utf8', function read(err, data) {
   processFile();
 });
 
+
 function processFile () {
+  console.log(questions);
   console.log(answers);
 }
+
+/*
+function MakeTests (var a, var b, var c)    //Where a is number of test versions. b is number of questions per test. and c is test question document.
+{
+  documentReader("./" + c);
+  var k = new Array();
+  var test = new Array();
+  var answersheet = new Array();
+
+  for (i = 0; i < a; i++)
+  {
+    for (i = 0; i < b; i++)
+    {
+      var random = Math.floor(Math.random() * (+(questions.length - 1) - +0) + +0);
+      while (k.includes(random))
+      {
+        random =  Math.floor(Math.random() * (+(questions.length - 1) - +0) + +0);
+      }
+      k.push(random);
+
+      test.push(questions[random]);
+      answersheet.push(answers[random]);
+
+    }
+    System.out.println(test);
+    System.out.println(answersheet);
+
+    console.log(test);
+    console.log(answersheet);
+  }
+}
+*/
