f38879d83422536636997c14937209d4a1acdb0e
armin ghobadi <armin.ghobadi95@gmail.com>
2019-02-21 11:24:47 -0500

    done

--- Main.js
+++ Main.js
@@ -1,18 +1,53 @@
 const fs = require('fs')
 
-const promiseRead = () => 
+/**
+ * USAGE:
+ * -v || --version => number of versions
+ * -f || --filename => output file name
+ * -q || --questions => the actual questionaire
+ * -n || --number => number of questions for each test
+ */
+ const readArgs = async () => {
+
+  const inputs = process.argv
+  
+  const versionIndex = inputs.indexOf('-v') + 1 || inputs.indexOf('--version')+1 || null
+  const outputFileNameIndex = inputs.indexOf('-f') + 1 || inputs.indexOf('--filename') + 1 || null
+  const questionareIndex = inputs.indexOf('-q') + 1 || inputs.indexOf('--questions') + 1 || null
+  const numOfQuestionsIndex = inputs.indexOf('-n') + 1 || inputs.indexOf('--number') + 1 || Infinity
+  if (!questionareIndex){
+    console.log('gimme something man')
+    return
+  }
+  const version = inputs[versionIndex] || 1
+  const outputFileName = inputs[outputFileNameIndex] || 'filename'
+  const questionare = inputs[questionareIndex]
+  const numberOfQuestions = inputs[numOfQuestionsIndex] || Infinity
+
+  const readQuestions = await parseQuestionare(questionare)
+
+  for (var i = 0 ; i < version ; i++ ) {
+    const { questionSheet, answerSheet } = parseString(readQuestions, numberOfQuestions)
+    writeToFile(questionSheet, `${outputFileName}${i}`)
+    writeToFile(answerSheet, `${outputFileName}answers${i}`)
+  }
+
+}
+
+const promiseRead = (fileName) => 
   new Promise( (resolve, reject) => {
-    fs.readFile('./questions.txt', 'utf8', (err, data)=>{
+    fs.readFile(`./${fileName}`, 'utf8', (err, data)=>{
       if (err) reject(err)
       resolve(data)
     })
   })
 
-const parseQuestionare = async () => {
+const parseQuestionare = async (fileName) => {
   try{
-    const rawData = await promiseRead()
+    const rawData = await promiseRead(fileName)
+    
     const processedData = rawData.split('\n').reduce((acc, item)=>{
-      //console.log(item)
+      
       if (!item){
         return acc
       }
@@ -21,11 +56,8 @@ const parseQuestionare = async () => {
         acc[acc.currentQuestion] = {}
       }
       else if ( acc.currentQuestion && acc[acc.currentQuestion]){
-        //console.log('asdfasdfasdfasfas')
         if (item[0] == '+'){
-          
           acc[acc.currentQuestion][item.substr(2)] = true
-          //console.log('asdfasdfasdfasfas')
         }
         else{
           acc[acc.currentQuestion][item] = false
@@ -33,16 +65,16 @@ const parseQuestionare = async () => {
       }
       return acc
     }, {currentQuestion: ''})
-    //console.log(processedData[processedData.currentQuestion])
     delete processedData.currentQuestion
-    parseString(processedData)
-  }catch(e){
+    return processedData 
+  }
+  catch(e){
     console.log(e)
   }
   
 }
 
-const shuffle = (array) => {
+const shuffle = (array, numberOfQuestions) => {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
   // While there remain elements to shuffle...
@@ -57,33 +89,39 @@ const shuffle = (array) => {
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-
+  if (numberOfQuestions < array.length) {
+    return array.slice(array.length - numberOfQuestions)
+  }
   return array;
 }
 
 
-const parseString = (dataObj) => {
+const parseString = (dataObj, numberOfQuestions) => {
   const questions = Object.keys(dataObj)
-  const randomizedQuestions = shuffle(questions)
+  const randomizedQuestions = shuffle(questions, numberOfQuestions)
   const questionSheet = []
   const answerSheet = []
 
-  randomizedQuestions.forEach( ( question )=>{
+  randomizedQuestions.forEach( ( question, questinoIndex )=>{
     const correspondigChoices = dataObj[question]
-    console.log('aaaaa',correspondigChoices)
+    questionSheet.push(question)
+    Object.keys(correspondigChoices).forEach((answer, answerIndex) => {
+      questionSheet.push(answer)
+      if ( correspondigChoices[answer] ){
+        answerSheet.push(`question number ${questinoIndex+1}: answer: ${answerIndex + 1}`)
+      }
+    })
   } )
 
-  // questions.reduce((acc, item, qindex)=>{
-  //   questionSheet.push(item)
-  //   Object.keys(answers[qindex]).forEach( (ans, aindex) => {
-  //     if (answers[qindex][ans]){
-  //       answerSheet.push(`q${qindex+1} answer: ${aindex}`)
-  //       questionSheet.push(`${aindex+1})${ans}`)
-  //     }
-  //   })
-  //   return acc
-  // },{})
-
+  return {
+    questionSheet: questionSheet.join('\n'),
+    answerSheet: answerSheet.join('\n')
+  }
 }
 
-parseQuestionare()
+const writeToFile = (data, fileName) => {
+  fs.writeFile(fileName, data, 'utf8', (err) => { if (err) console.log(err) })
+  return 'success!'
+}
+
+readArgs()
